555265d118f71a087267b897c7fa848cecffb8c7
null-zero <trueshrike7@gmail.com>
2023-11-28 12:28:55 -0600

    Added photos to gallery, added charity page, added sources, complete styling overhaul

---
+++ AWP-Individual-Project-1/Pages/Charity.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AWP_Individual_Project_1.Pages
+{
+    public class CharityModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- AWP-Individual-Project-1/Pages/Contact.cshtml.cs
+++ AWP-Individual-Project-1/Pages/Contact.cshtml.cs
@@ -7,10 +7,34 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace AWP_Individual_Project_1.Pages
 {
+    [BindProperties]
     public class ContactModel : PageModel
     {
+        public string fname { get; set; }
+        public string lname { get; set; }
+        public string email { get; set; }
+        public string message { get; set; }
+
+
+        public string fnameMsg { get; set; }
+        public string lnameMsg { get; set; }
+        public string emailMsg { get; set; }
+
+        public string messageMsg { get; set; }
+        public string userSubmitMsg { get; set; }
+
+        public string MsgColor;
         public void OnGet()
         {
+        }
+        public void OnPostSubmit()
+        {
+            MsgColor = "#59165a";
+            userSubmitMsg = "User submited data:";
+            fnameMsg = "First Name: " + fname;
+            lnameMsg = "Last Name: " + lname;
+            emailMsg = "Email: " + email;
+            messageMsg = "Message: " + message;
         }
     }
 }
